4647815133864808bd3b2c3a25ce76000f0e68b7
erickosma <erickosma@gmail.com>
2012-02-01 02:34:35 +0000

    sql

--- application/views/helpers/MyJavaScript.php
+++ application/views/helpers/MyJavaScript.php
@@ -15,7 +15,7 @@ class Zend_View_Helper_MyJavaScript extends Zend_View_Helper_HeadScript{
 		}
 		foreach ($scripts as $src=>$js) {
 			$item = new stdClass();
-			$item->type = 'text/javascript';
+			$item->typre = 'text/javascript';
 			$newname = 'js_' . md5('vcss'  . implode(',', $js) ) . '.js';
 			$this->process($js, $newname);
 			$item->attributes['src'] = $this->getMinUrl() . '/' . $newname;
